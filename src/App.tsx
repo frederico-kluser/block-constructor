57b080735ac43b8b1e5ac4af72e9b18051331f47
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-11-18 11:07:35 -0300

    refactor: fix Block resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Block from "./components/Block";
+import getRandomColor from "./utils/random-color";
 
 const App = () => (
-  <>
-    <Block />
-  </>
+  <div style={{
+    position: "absolute",
+    left: '50%',
+    top: '50%',
+  }}>
+    <Block backgroundColor={getRandomColor()} />
+  </div>
 );
 
 export default App;
